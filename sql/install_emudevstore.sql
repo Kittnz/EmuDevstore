6b420281372fa9fe8bba6ea1511dfe22fc26aaf5
Artister <alexei_93@yahoo.com>
2014-02-25 15:45:42 +0100

    Adding database structure, incomplete missing tables.

---
+++ sql/install_emudevstore.sql
@@ -0,0 +1,58 @@
+
+-- Creating structure for table categories
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` varchar(50) DEFAULT NULL,
+  `parent_id` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Creating structure for table ci_sessions
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` int(11) NOT NULL,
+  `ip_address` varchar(100) NOT NULL,
+  `user_agent` varchar(100) NOT NULL,
+  `last_activity` bigint(20) NOT NULL,
+  `user_data` varchar(100) NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Creating structure for table customers
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `real_name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `website` varchar(50) DEFAULT NULL,
+  `password_sha1` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Creating structure for table daily_income
+DROP TABLE IF EXISTS `daily_income`;
+CREATE TABLE IF NOT EXISTS `daily_income` (
+  `day` varchar(50) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Creating structure for table orders
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `customer_id` varchar(50) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Creating structure for table products
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `author_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `thumbnail` varchar(50) DEFAULT NULL,
+  `screenshot` varchar(50) DEFAULT NULL,
+  `download` varchar(50) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `paypal_email` varchar(50) DEFAULT NULL,
+  `is_unique` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
